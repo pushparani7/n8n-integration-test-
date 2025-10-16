fefac83a085c9e50d32e70b45907a75e7ccea1dd
PUSHPARANI.B <pushparanib7@gmail.com>
2025-10-16 17:37:28 +0530

    Switch from ES6 imports to CommonJS require

--- proxy/server.js
+++ proxy/server.js
@@ -1,8 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
+const path = require('path');
+const { fileURLToPath } = require('url');
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
