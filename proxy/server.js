20d459991b73255d09fd51a3d7c940626c72548b
PUSHPARANI.B <pushparanib7@gmail.com>
2025-10-16 17:53:19 +0530

    Fix route handling for frontend requests

--- proxy/server.js
+++ proxy/server.js
@@ -31,7 +31,7 @@ app.post("/api/webhook-test", async (req, res) => {
 
 // serve frontend build
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
